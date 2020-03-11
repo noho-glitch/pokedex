d990038378ee250e6fe451b8c579e590d5c618e1
Dave Nolan <dpn.nolan@gmail.com>
2020-03-11 12:48:47 -0400

    Updates

---
+++ precache-manifest.ab641a4738a07a1876def04aba170bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354ba59877ba256ec332e65c4b550569",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "2f4fe04927ead3830a55",
+    "url": "/pokedex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6427c27271caa076d618",
+    "url": "/pokedex/static/js/2.18d132c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedex/static/js/2.18d132c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4fe04927ead3830a55",
+    "url": "/pokedex/static/js/main.c7b59ff4.chunk.js"
+  },
+  {
+    "revision": "e9c8d32f045e9b89c48b",
+    "url": "/pokedex/static/js/runtime-main.54804aa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
